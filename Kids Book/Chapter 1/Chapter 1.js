63b4386c4858ce882a1a17c6bbaa7160270c831c
Kenny Sekhoela <133218651+Kenni95@users.noreply.github.com>
2024-01-23 00:04:24 +0200

    Add files via upload

---
+++ Kids Book/Chapter 1/Chapter 1.js
@@ -0,0 +1,37 @@
+// // Draw as many cats as you want!
+// var drawCats = function (howManyTimes) {
+//     for (var i = 0; i < howManyTimes; i++) {
+//     console.log(i + " =^.^=");
+//     }
+//     };
+//     drawCats(10); // You can put any number here instead of 10.
+
+//     // Draw as many cats as you want!
+// var drawCats = function (howManyTimes) {
+//     for (var i = 0; i < howManyTimes; i++) {
+//     console.log(i + " =^.^=");
+//     }
+//     };
+
+//     drawCats(5);
+
+// test 1
+console.log("test");
+
+// test 2
+var test2 = " hello world";
+console.log("Adding a string to with a variable = " + "test2" + test2);
+
+// test 3
+test2 = 3 * 2;
+console.log("Multiplying Two Numbers = " + "3 * 2: " + test2);
+
+// test 4
+var x = 2;
+var y = 3;
+var test4 = x * y;
+console.log("Multiplying Two numbers saved as letters = " + x * y);
+console.log("Multiplying then adding a two letters then = " + x + " * " + y + "= " + test4);
+console.log(test4);
+
+console.log("Printing two numbers being mutiplied = " + "567 * 55: " + (567 * 55)); 
